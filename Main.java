9912325bf8794d10a0627551158c8e92c94a5122
s92025592025 <s92025592025@gmail.com>
2016-03-19 14:03:04 -0700

    Final Version
    Passed UVa by 0.266

--- Main.java
+++ Main.java
@@ -5,14 +5,14 @@ import java.util.List;
 import java.util.ArrayList;
 
 class Main{
-	public static final Map<String, String> numberList = makeNumberList();
+	public static final Map<String, Integer> numberList = makeNumberList();
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		while(sc.hasNext()){
 			String input1 = sc.next();
 			String input2 = sc.next();
 			List<Integer> converted1 = findAllConvert(input1);
-			System.out.println(converted1.toString());
+			//System.out.println(converted1.toString());
 			int base1 = -1, base2 = -1;
 			for(int i = 2; i < 37; i++){
 				if(convert(i, input2) != null && converted1.contains(convert(i, input2))){
@@ -31,10 +31,10 @@ class Main{
 		}
 	}
 
-	public static Map<String, String> makeNumberList(){
-		Map<String, String> temp = new HashMap<String, String>();
+	public static Map<String, Integer> makeNumberList(){
+		Map<String, Integer> temp = new HashMap<String, Integer>();
 		for(int i = 65; i < 91; i++){
-			temp.put(Character.toString((char)i), Integer.toString(i - 55));
+			temp.put(Character.toString((char)i), i - 55);
 		}
 
 		return temp;
@@ -51,19 +51,24 @@ class Main{
 	}
 
 	public static Integer convert(int base, String input){
-		if(input.charAt(0) > 64 && input.charAt(0) < 91){
-			input = numberList.get(input);
-		}
-
 		int sum = 0;
 		int decimal = 1;
 		for(int i = input.length(); i > 0; i--){
-			if(Integer.parseInt(input.substring(i - 1, i)) >= base){
-				return null;
+			if(input.charAt(i - 1) > 64 && input.charAt(i - 1) < 91){
+				if(numberList.get(input.substring(i - 1, i)) >= base){
+					return null;
+				}
+
+				sum += numberList.get(input.substring(i - 1, i)) * decimal;
+			}else{
+				if(Integer.parseInt(input.substring(i - 1, i)) >= base){
+					return null;
+				}
+
+				sum += Integer.parseInt(input.substring(i - 1, i)) * decimal;
 			}
 
-			sum += Integer.parseInt(input.substring(i - 1, i)) * decimal;
-			decimal = decimal * base;
+			decimal *= base;
 		}
 
 		return sum;
